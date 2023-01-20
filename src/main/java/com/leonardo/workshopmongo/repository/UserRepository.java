df90471f0899acb1306f1aa68192fcbd674b3585
Leonardo <leonardohenriquetubero@gmail.com>
2023-01-20 19:07:06 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/leonardo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.leonardo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonardo.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
