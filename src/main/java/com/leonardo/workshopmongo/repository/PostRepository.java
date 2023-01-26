af82b72518fea58ba3eba51cfdb5ba6f709a8371
Leonardo <leonardohenriquetubero@gmail.com>
2023-01-26 18:20:32 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/leonardo/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.leonardo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonardo.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
